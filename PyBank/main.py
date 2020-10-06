437b682aa8bbfe0a301b17f5a36b047581d93529
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-10-06 02:23:39 -0400

    get average, test write to output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,27 +18,60 @@ budget_csv = os.path.join("Resources", "budget_data.csv")
 months = []
 changes = []
 ctr = 0
+change_ctr = 0
+net_total = 0
 monthly_change = 0
+
+greatest_increase = 0
+greatest_decrease = 0
+
+def average_change(numbers):
+    length = len(numbers)
+    total = 0
+    for number in numbers:
+        total += number
+    return total / length 
 
 # open file
 with open(budget_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     header = next(csvreader)
+    
     previous_record = next(csvreader)
     ctr += 1
+    net_total = int(previous_record[1])
     print(f"first record  {previous_record}")
+    #print(str(net_total))
 
     for row in csvreader:
         ctr += 1
+        net_total += int(row[1])
         months.append(row[0])
         monthly_change = int(row[1]) - int(previous_record[1])
         changes.append(monthly_change)
         previous_record = row
+        print(f"previous record  {previous_record}")
+
+        
+        change_ctr += 1
 
         #print(row)
         #print(row[0] + " " + str(row[1]))
 
-print(str(ctr)) #total number of months included in the data set
-print(changes[0])
-print(months[0])
+print(f"length of changes list {len(changes)}")
+print(f"total number of months included in the data set {ctr}") #total number of months included in the data set
+print("net total " + str(net_total)) #net total amount of "Profit/Losses"
 
+print(f"average = {average_change(changes)}")
+ 
+cleaned_file = zip(months,changes)
+output_file = os.path.join("output.csv")
+with open(output_file, "w", newline='') as datafile:
+    writer = csv.writer(datafile)
+    writer.writerows(cleaned_file)
+
+        # print(f"{changes[x]}")
+
+
+#print(months)
+#print(f"{months} {str(monthly_change)} ")
