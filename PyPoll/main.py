f356c48bdbe20c9e6fa42c40ac9cf52683534e10
tratnikc <68667850+tratnikc@users.noreply.github.com>
2020-10-07 19:24:31 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,7 +8,10 @@ import csv
 election_results = []
 candidates = []
 candidate_votes = {}
+tally_results = []
+poll_results = []
 total_votes = 0
+
 
 def percentage_vote(counts):
     pct = 0
@@ -21,9 +24,10 @@ def tally_votes():
     for running_man in candidates:
         vote_count = election_results.count(running_man)
         vote_pct = percentage_vote(vote_count)
-        #print(f"{vote_pct}")
-        candidate_votes.update({running_man : [vote_pct, vote_count]}) 
-        #print(f"{running_man}: ({election_results.count(running_man)})")
+        candidate_votes = {"name" : running_man,
+                            "vote_pct" : vote_pct,
+                            "vote_count" : vote_count}
+        tally_results.append(candidate_votes)
 
 # path to collect data from Resources folder
 votes_csv = os.path.join("Resources", "election_data.csv")
@@ -33,17 +37,24 @@ with open(votes_csv) as csvfile:
     header = next(csvreader)
     for row in csvreader:
         election_results.append(row[2])
-        #if candidates.count(row[2]) == 0:
-        #    candidates.append(row[2])
 
 candidates = set(election_results)
 total_votes = len(election_results)
-
 tally_votes()
-print(candidate_votes)
 
-print("---------------------")
+sorted_results = sorted(tally_results, key=lambda item:item['vote_count'], reverse=True)
 
+print(sorted_results[0]["name"])
+
+header = "Election Results"
+separator = "-----------------------"
+total_line = f"Total Votes: {total_votes}"
+for line in sorted_results:
+    print(line["name"])
+
+
+
+"""
 khan_votes = election_results.count("Khan")
 correy_votes = election_results.count("Correy")
 li_votes = election_results.count("Li")
@@ -55,6 +66,6 @@ print(f"Khan: {khan_votes}")
 print(f"Correy: {correy_votes}")
 print(f"Li: {li_votes}")
 print(f"O'Tooley: {otooley_votes}")
-    
+"""    
 
 
